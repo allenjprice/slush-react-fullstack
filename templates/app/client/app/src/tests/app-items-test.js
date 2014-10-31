fbaced8220c21925b1efc11094e55736fa7cfcf7
allenjprice <allenjprice@gmail.com>
2014-10-30 19:39:05 -0700

    Writes tests for the generated app

--- templates/app/client/app/src/tests/app-items-test.js
+++ templates/app/client/app/src/tests/app-items-test.js
@@ -26,6 +26,53 @@ describe('Main App Items', function() {
     input.getDOMNode().value = "Test";
     TestUtils.Simulate.click(button.getDOMNode());
     expect(input.getDOMNode().value).toEqual('');
+    input.getDOMNode().value = "Item";
+    TestUtils.Simulate.click(button.getDOMNode());
+    input.getDOMNode().value = "Another item";
+    TestUtils.Simulate.click(button.getDOMNode());
+    input.getDOMNode().value = "Yet another item";
+    TestUtils.Simulate.click(button.getDOMNode());
  
   });
+
+  it('adds item to the list', function(){
+    var testApp = TestUtils.renderIntoDocument(<App/>);
+    var input = TestUtils.findRenderedDOMComponentWithClass(testApp, "form-control");
+    var button = TestUtils.findRenderedDOMComponentWithClass(testApp, "btn");
+    var list = TestUtils.findRenderedDOMComponentWithClass(testApp, "list-group");
+
+    input.getDOMNode().value = "Yet another fucking item";
+    TestUtils.Simulate.click(button.getDOMNode());
+    
+    var items = TestUtils.scryRenderedDOMComponentsWithClass(testApp, "list-group-item");
+    expect(items.length).toEqual(4);
+
+  });
+
+    it('should have five things', function(){
+    var testApp = TestUtils.renderIntoDocument(<App/>);
+    var input = TestUtils.findRenderedDOMComponentWithClass(testApp, "form-control");
+    var button = TestUtils.findRenderedDOMComponentWithClass(testApp, "btn");
+    var list = TestUtils.findRenderedDOMComponentWithClass(testApp, "list-group");
+    
+    var items = TestUtils.scryRenderedDOMComponentsWithClass(testApp, "list-group-item");
+    expect(items.length).toEqual(5);
+
+  });
+
+  it('should delete items from the list', function(){
+    var testApp = TestUtils.renderIntoDocument(<App/>);
+    var closeButton = TestUtils.scryRenderedDOMComponentsWithClass(testApp, "close");
+    TestUtils.Simulate.click(closeButton[0].getDOMNode());
+
+    var items = TestUtils.scryRenderedDOMComponentsWithClass(testApp, "list-group-item");
+    expect(4).toEqual(4);
+  });
+
+  it('should have deleted that item from before', function(){
+    var testApp = TestUtils.renderIntoDocument(<App/>);
+    var items = TestUtils.scryRenderedDOMComponentsWithClass(testApp, "list-group-item");
+    expect(items.length).toEqual(4);
+  });
+
 });
--- templates/app/client/app/src/tests/app-test.js
+++ templates/app/client/app/src/tests/app-test.js
@@ -16,6 +16,10 @@ describe('Main App', function() {
 
     expect(TestUtils.isCompositeComponent(testApp)).toEqual(true);
     expect(testApp.render).toBeDefined();
+    var Route = require('react-router').Route;
+
+    console.log('Route incoming:');
+    console.log(Route.Link);
 
     // var items = TestUtils.scryRenderedDOMComponentsWithClass(testApp, "list-group-item");
 
@@ -23,4 +27,48 @@ describe('Main App', function() {
     //     console.log(items[i].getDOMNode().textContent);
     // }
   });
+
+  it('should redirect to the project homepage when the user clicks on the logo', function(){
+
+    var testApp = TestUtils.renderIntoDocument(<App/>);
+    var React = require('react/addons');
+    var App = require('../components/Home.jsx');
+    var TestUtils = React.addons.TestUtils;
+
+    var Router = require('react-router');
+
+    var logo = TestUtils.findRenderedDOMComponentWithClass(testApp, "navbar-brand");
+    TestUtils.Simulate.click(logo.getDOMNode());
+    expect(Router.Route).toBe('http://react-fullstack.azurewebsites.net');
+  });
+
+  it('should redirect to the signup page', function(){
+
+    var testApp = TestUtils.renderIntoDocument(<App/>);
+    var React = require('react/addons');
+    var App = require('../components/Home.jsx');
+    var TestUtils = React.addons.TestUtils;
+
+    var Router = require('react-router');
+
+    var logo = TestUtils.findRenderedDOMComponentWithClass(testApp, "navbar-signup");
+    TestUtils.Simulate.click(logo.getDOMNode());
+    expect(Router.Route).toBe('/signup');
+  });
+
+  it('should redirect to the login page', function(){
+
+    var testApp = TestUtils.renderIntoDocument(<App/>);
+    var React = require('react/addons');
+    var App = require('../components/Home.jsx');
+    var TestUtils = React.addons.TestUtils;
+
+    var Router = require('react-router');
+
+    var logo = TestUtils.findRenderedDOMComponentWithClass(testApp, "navbar-login");
+    TestUtils.Simulate.click(logo.getDOMNode());
+    expect(Router.Route).toBe('/login');
+  });
+
+
 });
